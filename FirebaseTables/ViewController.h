a422e9c51be030b0705e43314df53924e08a07f0
Donovan Cotter <DetroitLabs@DonovanDLabsMacBookPro.local>
2016-06-01 09:49:22 -0400

    first commit

---
+++ FirebaseTables/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FirebaseTables
+//
+//  Created by DetroitLabs on 6/1/16.
+//  Copyright © 2016 DetroitLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FirebaseTables/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  FirebaseTables
+//
+//  Created by DetroitLabs on 6/1/16.
+//  Copyright © 2016 DetroitLabs. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
